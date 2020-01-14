c6e7d79aaeee259efdd38800cf431cdd13636671
Ricardo <richarin@hotmail.com.ar>
2020-01-14 08:02:48 -0500

    Home
    Cambio de contenedores Noticias y Objetos Recuperados

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,6 +9,7 @@
     <img src="{{URL::asset('images/principal/espe.png')}}" >
 </div> -->
 
+<<<<<<< Updated upstream
 <div class="container mt-3 bg-white custom-font">
     <div class="row pt-4">
         <div class="col-12 col-sm-6">
@@ -33,10 +34,49 @@
                             <p class="card-text"><small class="text-muted">{{$noticia -> NOT_FECHA_INICIO}}</small></p>
                         </div>
                     </div>
+=======
+<div class="container container-main bg-white">
+    <div class="content_area">
+        <div class="col-sm-6">
+            <div class="left_coloum floatleft">
+                <div class="single_left_coloum_wrapper">
+                    <h2 class="title">Noticias</h2>
+                    @forelse($noticias as $noticia)
+                    <div class="single_left_coloum floatleft">
+                        <img class="noticia" src="{{$noticia -> NOT_IMAGEN}}"/>
+                        <h3>{{$noticia -> NOT_TITULO}}</h3>
+                        <p>{{$noticia -> NOT_DESCRIPCION}}</p>
+                        
+                        @if(Auth::check())
+                            <a class="readmore" href="{{url('home/noticiadetail/'.$noticia->NOT_CODIGO.'')}}">leer mas</a>    
+                        @else
+                            <a class="readmore" href="{{url('/noticiadetail/'.$noticia->NOT_CODIGO.'')}}">leer mas</a>
+                        @endif
+                    </div>    
+                    @empty
+
+                    @endforelse  
+>>>>>>> Stashed changes
                 </div>
                 @endforeach  
             </div>
         </div>
+<<<<<<< Updated upstream
+=======
+        
+        <div class="col-sm-6">
+            <div class="left_coloum floatright">
+                <div class="single_left_coloum_wrapper">
+                    <h2 class="title">Objetos Recuperados En El Laboratorio</h2>
+                    @forelse($objetos as $objeto)
+                    <div class="single_left_coloum floatright">
+                        <img class="noticia" src="{{$objeto ->  OBR_IMAGEN}}"/>
+                        <h3>{{$objeto -> OBR_NOMBRE}}</h3>
+                        <p>{{$objeto -> OBR_DESCRIPCION}}</p>
+                        
+                    </div>    
+                    @empty
+>>>>>>> Stashed changes
 
         <div class="col-12 col-sm-6">
             <h2 class="title">Objetos Recuperados En El Laboratorio</h2>
